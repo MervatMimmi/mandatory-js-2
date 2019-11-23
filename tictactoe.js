21e1b1b09ab32621a6ad52bd51f38840d0f840a6
Mervat Hassan <mervat.hassan@gmail.com>
2019-11-23 14:19:44 +0100

    second committed

--- tictactoe.js
+++ tictactoe.js
@@ -1,23 +1,15 @@
 let containers = document.querySelectorAll('.container');
+let countSelectedContainers = 0;
 let player = 'X';
 let winner = false;
-let winComb = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8]
-];
 startGame();
 
 //====== select the container and add event listener for each container.=======
 function startGame() {
-for (var i = 0; i < containers.length; i++) {
-  let container = containers[i];
-  container.addEventListener('click', onClick);
+countSelectedContainers = 0;
+  for (var i = 0; i < containers.length; i++) {
+    let container = containers[i];
+    container.addEventListener('click', onClick);
   }
 }
 //console.log(containers.length);
@@ -29,41 +21,41 @@ function onClick(e) {
   if (container.innerHTML === '') {
       player = player ==='X' ? '0': 'X';
       container.innerHTML = player;
+      countSelectedContainers++;
   }
   rowInLine();
 }
-
 //=============================== 3 in a row ===================================
 function rowInLine() {
+  let winComb = [[0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 4, 8],[2, 4, 6],[0, 3, 6],[1, 4, 7],
+  [2,  5, 8]];
   let containInRow = containers;
   winComb.forEach(function(containComb){
   let positionZero = containInRow[containComb[0]].innerHTML;
-  //console.log(containComb[0]);
   let positionOne = containInRow[containComb[1]].innerHTML;
   let positionTwo = containInRow[containComb[2]].innerHTML;
   //console.log(positionZero + ':' + positionOne + ':' + positionTwo); //if you want to se how many in a row
   winner = positionZero !== '' && positionZero === positionOne && positionOne === positionTwo;
   //console.log(winner); if you want to se if its true or false
-    if (winner === true) {
-    gameOver();
-    time();
+    if (winner === true || countSelectedContainers === 9 ) {
+      player = positionZero;
+      noMoreMove();
+      modal();
     }
   });
 }
-
 //=================== no more moves is allowed================================
-function gameOver() {
-    for (var i = 0; i < containers.length; i++) {
-      let container = containers[i];
-      container.removeEventListener('click', onClick);
-    }
+function noMoreMove() {
+  for (var i = 0; i < containers.length; i++) {
+    let container = containers[i];
+    container.removeEventListener('click', onClick);
+  }
 }
-
 //===================== Do you want to play again =============================
 function modal() {
   let modal = document.querySelector('.modal');
   modal.style.display= 'flex';
-  let text = document.querySelector('.modal-popup');
+  gameMsg();
 
   document.querySelector('.button-no').addEventListener('click', function(){
   modal.style.display='none';
@@ -77,10 +69,12 @@ function modal() {
   startGame();
   });
 }
-
-  ///=======================setTimeout=====================
-  function time() {
-    setTimeout(function() {
-      modal();
-    },1500);
+//==============================Message in the modal============================
+function gameMsg() {
+  let text = document.getElementById('text');
+  if (countSelectedContainers === 9 && winner === false) {
+      return text.innerHTML = 'There is no winner, would you like to play again?';
+  } else if (winner === true) {
+      return text.innerHTML ='Congratulations ' + player + ', you won ! Do you want to play again ?';
   }
+}
